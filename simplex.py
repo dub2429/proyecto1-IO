3fc2969855911d4aebf74ac576bce4c86be3d489
Fabian Vives <f.vives99@gmail.com>
2021-08-31 13:57:33 -0600

    Creacion matriz

--- simplex.py
+++ simplex.py
@@ -1,12 +1,97 @@
 import numpy as np
 
 
-matriz = [[-3, -5, 0, 0, 0, 0],
- [1, 0, 1 , 0, 0 , 4],
- [0, 2, 0, 1,0,12],
- [3, 2, 0, 0, 1, 18]]
+import numpy as np
+
+def leerDocumento():
+    with open('archivo1.txt') as documento:
+        contenido = documento.read()
+        #print(contenido.rstrip())
+    arreglo = contenido.split("\n")
+    arregloMatriz = []
+    for i in range(len(arreglo)):
+        arregloMatriz.append(arreglo[i].split(","))
+    print(arregloMatriz)
+    return crear_matriz(arregloMatriz)
+
+i = 0
+def crearMatriz(contenido):
+    print(contenido)
+
+
+
+
+
+def crear_matriz(matrizDocumento):
+    numero_varZ = int(matrizDocumento[0][2])
+    numero_inec = int(matrizDocumento[0][3])
+    num_filas = numero_inec + 2
+    num_colum = numero_inec + numero_varZ + 2
+    matriz = []
+    variablesBasicas = numero_inec+numero_varZ
+    variablesNoBasicas = numero_varZ
+    variablesNoBasicasIngresar = numero_varZ
+
+    print("#Variables Básicas: "+ str(variablesBasicas-variablesNoBasicas)+ "   #Variables No Básicas: " + str(variablesNoBasicas))
+    for i in range(num_filas):
+        matriz.append([])
+        for j in range(num_colum):
+                matriz[i].append(0)
+    #print(matriz)
+    
+    for i in range(num_filas):
+        if i == 0:
+            for j in range(num_colum):
+                if(j==0):
+                    matriz[i][j] = "VB"
+                elif(j==(num_colum-1)):
+                    matriz[i][j] = "LD"
+                else:
+                    matriz[i][j] = "X"+str(j)
+        elif i == 1:
+            elemento = 0
+            for j in range(num_colum):
+                if j == 0:
+                    matriz[i][j] = "U"
+                elif elemento<len(matrizDocumento[i]) and matrizDocumento[i][elemento]:
+                     matriz[i][j] = -float(matrizDocumento[i][elemento])
+                     elemento += 1
+                
+        else:
+            elemento = 0
+            for j in range(num_colum):
+                if j == 0:
+                    matriz[i][j] = "X"+str(i+1) #Cambiar y meter las variables básicas del arreglo
+                elif elemento<len(matrizDocumento[i]) and matrizDocumento[i][elemento] != "<=" :
+                     matriz[i][j] = float(matrizDocumento[i][elemento])
+                     elemento += 1
+                #elif matrizDocumento[i][j] == "<=":
+                    #elemento += 1
+                if j == variablesNoBasicasIngresar+1:
+                    matriz[i][j] = 1
+                    
+                else:
+                    if j == len(matriz[i])-1:
+                        matriz[i][j] = float(matrizDocumento[i][len(matrizDocumento[i])-1])
+                        variablesNoBasicasIngresar += 1
+                    
+
+    print(matriz)
+    return matriz
+
+
+
+matriz = leerDocumento()
 matriz_np = np.array(matriz)
-matriz_np=np.float64(matriz_np)
+
+#np.array([[15,26,2,17,22],[13,8,9,3,4]])
+elemento= matriz_np[1][1]
+print(float(elemento)*5)
+
+matrix = matriz_np[1:, 1:]
+matrix=np.float64(matrix)
+
+
 filaPivote = []
 
 def cambiar_fila(matriz,fila, numeroFilaPivote):
@@ -37,7 +122,7 @@ def determinar_solucion(matriz, iteracion):
             posicionMenorEnColumna = x
 
     if(menorActual >= 0):
-        #print("\nMatriz Final: \n", matriz, "\nU: ", matriz[0][len(matriz[0])-1] ,"\nSolución: ", "Sin Terminar")
+        print("\nMatriz Final: \n", matriz, "\nU: ", matriz[0][len(matriz[0])-1] ,"\nSolución: ", "Sin Terminar")
         texto = "\n\nMatriz Final: \n"+ str(matriz)+ "\nU: "+ str(matriz[0][len(matriz[0])-1]) +"\nSolución: "+ "Sin Terminar"
         return escribir(texto)
     else:                
@@ -45,6 +130,7 @@ def determinar_solucion(matriz, iteracion):
         columnaMenor = matriz[:,posicionMenorEnColumna]
         columnaResultado = matriz[:,(len(matriz[0]))-1]               
         numeroFila = 0
+        textoColumnaPivote = "\nColumna Pivote: "+ str(columnaMenor) 
         
         #Acá sacamos al pivote y los respectivos valores de la división de LD / columna pivote
         if iteracion == 0:
@@ -82,7 +168,7 @@ def determinar_solucion(matriz, iteracion):
                 cambiar_fila(matriz, filaPivoteNueva, numeroFila)
                 nuevaFila = []                    
             y+=1
-        texto = "\n\nPivote: " +str(pivote) +"\nCambiando Fila: Pivote" +"\nFila Pivote: " + str(filaAntigua) + "\nNueva Fila pivote: " + str(filaPivoteNueva) + "\nNueva Matriz:\n"+ str(matriz) +  "\n\n\n"
+        texto = "\n\nPivote: " +str(pivote) +textoColumnaPivote+"\nCambiando Fila: Pivote" +"\nFila Pivote: " + str(filaAntigua) + "\nNueva Fila pivote: " + str(filaPivoteNueva) + "\nNueva Matriz:\n"+ str(matriz) +  "\n\n\n"
         escribir(texto)
         filaAntigua = []
         while m < len(matriz):
@@ -102,10 +188,9 @@ def determinar_solucion(matriz, iteracion):
                 filaAntigua = [] 
             m += 1
         
-    print(matriz)
+    #print(matriz)
     determinar_solucion(matriz, 1)
     
+
             
-determinar_solucion(matriz_np, 0)
-
-
+determinar_solucion(matrix, 0)
